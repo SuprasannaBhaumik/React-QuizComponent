7f342f593c311517bbb26417cd911143d49b4e2f
SuprasannaBhaumik <suprasanna.bhaumik86@gmail.com>
2018-06-22 12:25:44 +0530

    quizend component developed
    quizend component developed

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
 import React, {Component } from 'react';
 import QuizQuestion from './QuizQuestion';
-
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -14,13 +14,14 @@ class Quiz extends Component {
 	}
 
 	render() {
+		const isQuizEnd = this.state.quiz_position -1 === quizData.quiz_questions.length ? <QuizEnd/> : 
+		<QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position -1]} />;
 		return (
 			<div>
-				<QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position -1]} />
+				{isQuizEnd}
 			</div>
 		)
 	}
 }
-
 
 export default Quiz;
